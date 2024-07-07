225de4930003ea0ce3003e11cae1072f4411a1e2
nex <nextso1216@gmail.com>
2024-07-07 10:33:41 +0800

    fix glob var prob

--- app/models/analysis/ana.py
+++ app/models/analysis/ana.py
@@ -1,7 +1,8 @@
 from app.models.lark_bot.msg import alert
 from datetime import datetime
+import app.config.config
 
 
 def start_ana():
+    app.config.config.sync_once_last_execute_time = datetime.now()
     # alert("全站最近[X]筆RTP超過 105%","起始注單:d4419c87\n結束注單:4c623bcf")
-    sync_once_last_execute_time = datetime.now()
--- app/models/scheduler/sched.py
+++ app/models/scheduler/sched.py
@@ -1,7 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime, timedelta
 from app.utils.utils import time_fmt
-from app.config.config import sync_once_last_execute_time
+import app.config.config as config
 
 
 # 一個單例排程器 每隔 interval 秒數 固定執行一次 fn 函數 並更新最後執行時間 檢查時允許誤差在buffer內
@@ -10,7 +10,6 @@ class Once:
         self.interval = interval # 初始化檢查間隔(秒為單位)
         self.scheduler = BackgroundScheduler()
         self.scheduler.add_job(fn,'interval',seconds= self.interval)
-        self.lasttime = sync_once_last_execute_time # 初始化最近執行時間
         self.time_with_buffer = buffer + interval
         self.initflag = False # 初始化旗標
 
@@ -32,8 +31,9 @@ class Once:
     def is_alive(self):
         if self.initflag:
             now = datetime.now()
-            diff = now - self.lasttime
+            lasttime = config.sync_once_last_execute_time
+            diff = now - lasttime
             if diff.seconds > self.time_with_buffer :
-                return "[請注意排程器可能故障][上次執行時間:{tm}][相距:{df}秒]".format(tm= time_fmt(self.lasttime), df= diff.seconds)
-            return "[正常運行中][上次執行時間:{tm}][相距:{df}秒]".format(tm= time_fmt(self.lasttime), df= diff.seconds)
+                return "[請注意排程器可能故障][上次執行時間:{tm}][相距:{df}秒]".format(tm= time_fmt(lasttime), df= diff.seconds)
+            return "[正常運行中][上次執行時間:{tm}][相距:{df}秒]".format(tm= time_fmt(lasttime), df= diff.seconds)
         return "[無效操作:未初始化]"
